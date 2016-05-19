2eaf4b94a7410979c3559881925e0b75eef3f682
Robijn Vogel <robijn.bird@gmail.com>
2016-05-19 10:42:03 +0200

    Backup

--- ThutCore/src/main/java/thut/core/common/items/ItemSpreader.java
+++ ThutCore/src/main/java/thut/core/common/items/ItemSpreader.java
@@ -1,17 +1,20 @@
 package thut.core.common.items;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockLiquid;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
 import thut.api.ThutItems;
+import thut.api.maths.Vector3;
 import thut.core.common.ThutCore;
 
-public class ItemSpreader extends Item
-{
+public class ItemSpreader extends Item {
 
-    public ItemSpreader()
-    {
+    public ItemSpreader() {
         super();
         this.setHasSubtypes(true);
         this.setUnlocalizedName("spreader");
@@ -19,21 +22,19 @@ public class ItemSpreader extends Item
         ThutItems.spreader = this;
     }
 
-    // TODO make this work again
-    public boolean onItemUse(ItemStack itemstack, EntityPlayer player, World worldObj, int x, int y, int z, int side,
-            float hitX, float hitY, float hitZ)
-    {
-        // Block b = worldObj.getBlock(x, y, z);
-        // if(b instanceof BlockFluid && !worldObj.isRemote)
-        // {
-        // BlockFluid fluid = (BlockFluid) b;
-        // if(!fluid.solid)
-        // {
-        // fluid.trySpread(worldObj, new Vector3(x, y, z), true);
-        // return true;
-        // }
-        // }
-
+    @Override
+    public boolean onItemUse(ItemStack itemstack, EntityPlayer player, World worldObj, BlockPos pos, EnumFacing side,
+            float hitX, float hitY, float hitZ) {
+        Block b = worldObj.getBlockState(pos).getBlock();
+        if (b instanceof BlockLiquid && !worldObj.isRemote) {
+            BlockLiquid fluid = (BlockLiquid) b;
+            if (!fluid.isSideSolid(worldObj, pos, side)) {
+                Vector3 spreadVector = Vector3.getNewVector();
+                spreadVector.set(hitX, hitY, hitZ);
+                fluid.trySpread(worldObj, spreadVector, true);
+                return true;
+            }
+        }
         return false;
     }
 }
